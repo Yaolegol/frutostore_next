0e42f4101ad95cc10c7792d8a6617fa020d9711d
Yaolegol <m160160@yandex.ru>
2024-11-23 18:32:42 +0700

    Layout. Header

--- src/modules/Layout/components/Header/index.tsx
+++ src/modules/Layout/components/Header/index.tsx
@@ -7,23 +7,25 @@ import style from './index.module.scss';
 export const Header: FC = () => {
     return (
         <header className={style.index}>
-            <div className={style.areaLeft}>
-                <Logo />
-            </div>
-            <nav className={style.nav}>
-                <Link className={style.navItem} href="/">
-                    Главная
-                </Link>
-                <Link className={style.navItem} href="/catalog">
-                    Каталог
-                </Link>
-                <Link className={style.navItem} href="/cart">
-                    Корзина
-                </Link>
-            </nav>
-            <div className={style.areaRight}>
-                <Icon className={style.profileIcon} name="profile" />
-                <Icon className={style.cartIcon} name="cart" />
+            <div className={style.container}>
+                <div className={style.areaLeft}>
+                    <Logo />
+                </div>
+                <nav className={style.nav}>
+                    <Link className={style.navItem} href="/">
+                        Главная
+                    </Link>
+                    <Link className={style.navItem} href="/catalog">
+                        Каталог
+                    </Link>
+                    <Link className={style.navItem} href="/cart">
+                        Корзина
+                    </Link>
+                </nav>
+                <div className={style.areaRight}>
+                    <Icon className={style.profileIcon} name="profile" />
+                    <Icon className={style.cartIcon} name="cart" />
+                </div>
             </div>
         </header>
     );
