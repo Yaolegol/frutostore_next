f849f39ac031d416b0cf6f91a0704e9ca98eb1c3
Yaolegol <m160160@yandex.ru>
2024-11-30 18:10:30 +0700

    Карточка товара. Добавлен counter

--- src/modules/ProductCard/components/Counter/index.tsx
+++ src/modules/ProductCard/components/Counter/index.tsx
@@ -1,6 +1,52 @@
-import { FC } from 'react';
+'use client';
+
+import { styles } from '@/helpers/styles';
+import { ChangeEvent, FC, useState } from 'react';
 import style from './index.module.scss';
 
 export const Counter: FC = () => {
-    return <div className={style.index}>test</div>;
+    const [value, setValue] = useState('');
+
+    const handleDecrement = () => {
+        const val = Number(value) - 1;
+
+        setValue(String(val));
+    };
+
+    const handleIncrement = () => {
+        const val = Number(value) + 1;
+
+        setValue(String(val));
+    };
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const val = e.target.value;
+
+        setValue(val);
+    };
+
+    return (
+        <div className={style.index}>
+            <button
+                className={styles(style.button, style.buttonLeft)}
+                onClick={handleDecrement}
+                type="button"
+            >
+                -
+            </button>
+            <input
+                className={style.input}
+                onChange={handleChange}
+                type="text"
+                value={value}
+            />
+            <button
+                className={styles(style.button, style.buttonRight)}
+                onClick={handleIncrement}
+                type="button"
+            >
+                +
+            </button>
+        </div>
+    );
 };
