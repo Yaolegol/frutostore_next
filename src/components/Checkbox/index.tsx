a468e8d3347d280c68cc3c1b9a651bb9f2a2f113
Yaolegol <m160160@yandex.ru>
2025-01-01 23:02:30 +0700

    Добавлен Checkbox

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,15 +1,25 @@
-import { FC } from 'react';
+import { Icon } from '@/components/Icon';
+import { ChangeEvent, FC } from 'react';
 import style from './index.module.scss';
 
 interface IProps {
+    isChecked?: boolean;
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void;
     text: string;
 }
 
-export const Checkbox: FC<IProps> = ({ text }) => {
+export const Checkbox: FC<IProps> = ({ isChecked, onChange, text }) => {
     return (
         <label className={style.index}>
-            <input className={style.input} type="checkbox" />
-            <div className={style.checkmarkContainer}></div>
+            <input
+                checked={isChecked}
+                className={style.input}
+                onChange={onChange}
+                type="checkbox"
+            />
+            <div className={style.checkmarkContainer}>
+                <Icon className={style.checkmarkIcon} name="checkmark" />
+            </div>
             <div className={style.text}>{text}</div>
         </label>
     );
