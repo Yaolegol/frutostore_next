d7c843f3b1b2dd734fc88e31e93fe8ad3c3446bf
Yaolegol <m160160@yandex.ru>
2025-01-02 16:43:59 +0700

    Добавлен селект сортировки

---
+++ src/modules/Sort/constants/index.ts
@@ -0,0 +1,10 @@
+export const SORT_OPTIONS = [
+    {
+        label: 'По возрастанию',
+        value: 'asc',
+    },
+    {
+        label: 'По убыванию',
+        value: 'desc',
+    },
+];
--- src/modules/pages/Catalog/index.tsx
+++ src/modules/pages/Catalog/index.tsx
@@ -4,6 +4,7 @@ import { Filters } from '@/modules/Filters/components/Filters';
 import { Pagination } from '@/modules/pages/Catalog/components/Pagination';
 import { ProductList } from '@/modules/pages/Catalog/components/ProductList';
 import { ShowMoreButton } from '@/modules/pages/Catalog/components/ShowMoreButton';
+import { SortSelect } from '@/modules/Sort/components/SortSelect';
 import { FC } from 'react';
 import style from './index.module.scss';
 
@@ -16,7 +17,12 @@ export const Catalog: FC = () => {
                     <Filters />
                 </div>
                 <div>
-                    <ProductList />
+                    <div className={style.sortContainer}>
+                        <SortSelect />
+                    </div>
+                    <div className={style.productsContainer}>
+                        <ProductList />
+                    </div>
                     <div>
                         <Pagination />
                     </div>
