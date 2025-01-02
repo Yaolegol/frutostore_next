42260685c4bd1e02362e97a758c7570dfa1c72f8
Yaolegol <m160160@yandex.ru>
2025-01-02 23:47:25 +0700

    Настроен роут cart

--- src/modules/pages/Cart/index.tsx
+++ src/modules/pages/Cart/index.tsx
@@ -2,5 +2,10 @@ import { FC } from 'react';
 import style from './index.module.scss';
 
 export const Cart: FC = () => {
-    return <div className={style.index}>test</div>;
+    return (
+        <div className={style.index}>
+            <h1>Корзина</h1>
+            <div>test</div>
+        </div>
+    );
 };
