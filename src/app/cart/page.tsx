237d505c07dba6152404631257e5c38eeb75a959
Yaolegol <m160160@yandex.ru>
2025-01-03 00:51:02 +0700

    Рефакторинг модуля Cart

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -2,27 +2,21 @@ import { getServerLangData } from '@/helpers/lang/server';
 import { LayoutProvider } from '@/modules/Layout/provider';
 import { Cart } from '@/modules/pages/Cart';
 import { CartPageProvider } from '@/modules/pages/Cart/provider';
-import { CatalogService } from '@/modules/pages/Catalog/service';
 import { Metadata, NextPage } from 'next';
 
 export const metadata: Metadata = {
     title: 'Fruits store',
 };
 
-const catalogService = new CatalogService();
-
 const CartPage: NextPage = async () => {
     const { defaultLangOption, defaultLangText } = await getServerLangData();
-    const data = await catalogService.getProducts({
-        perPage: '50',
-    });
 
     return (
         <LayoutProvider
             defaultLangOption={defaultLangOption}
             defaultLangText={defaultLangText}
         >
-            <CartPageProvider products={data?.data ?? []}>
+            <CartPageProvider>
                 <Cart />
             </CartPageProvider>
         </LayoutProvider>
--- src/modules/pages/Cart/provider/index.tsx
+++ src/modules/pages/Cart/provider/index.tsx
@@ -2,16 +2,39 @@
 
 import { CartContext } from '@/modules/Cart/context';
 import { CartPageContext } from '@/modules/pages/Cart/context';
+import { CatalogService } from '@/modules/pages/Catalog/service';
 import { ICatalogProduct } from '@/modules/pages/Catalog/types';
-import { FC, ReactNode, useContext, useMemo } from 'react';
+import {
+    FC,
+    ReactNode,
+    useCallback,
+    useContext,
+    useEffect,
+    useMemo,
+    useState,
+} from 'react';
 
 interface IProps {
     children: ReactNode;
-    products: ICatalogProduct[];
 }
 
-export const CartPageProvider: FC<IProps> = ({ products, children }) => {
+const catalogService = new CatalogService();
+
+export const CartPageProvider: FC<IProps> = ({ children }) => {
+    const [products, setProducts] = useState<ICatalogProduct[]>([]);
     const { cart } = useContext(CartContext);
+
+    const getProducts = useCallback(async () => {
+        const data = await catalogService.getProducts({
+            perPage: '50',
+        });
+
+        setProducts(data?.data ?? []);
+    }, []);
+
+    useEffect(() => {
+        getProducts();
+    }, [getProducts]);
 
     const value = useMemo(() => {
         if (!products.length) {
