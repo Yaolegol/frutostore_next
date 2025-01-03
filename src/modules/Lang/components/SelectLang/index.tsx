cbdfbfa4d63aab4aff2d711d6e0e8695e53b0885
Yaolegol <m160160@yandex.ru>
2025-01-04 01:17:16 +0700

    Рефакторинг SelectLang

--- src/modules/Lang/components/SelectLang/index.tsx
+++ src/modules/Lang/components/SelectLang/index.tsx
@@ -3,10 +3,22 @@
 import { IOption, Select } from '@/components/Select';
 import { LANG_OPTIONS } from '@/modules/Lang/constants';
 import { LangContext } from '@/modules/Lang/context';
-import { FC, useCallback, useContext } from 'react';
+import { FC, useCallback, useContext, useMemo } from 'react';
 
 export const SelectLang: FC = () => {
-    const { onLangChange } = useContext(LangContext);
+    const { langOption, onLangChange } = useContext(LangContext);
+
+    const defaultSelectedOptionIndex = useMemo(() => {
+        const index = LANG_OPTIONS.findIndex(
+            ({ value }) => value === langOption?.value,
+        );
+
+        if (index === -1) {
+            return 0;
+        }
+
+        return index;
+    }, [langOption]);
 
     const handleSelect = useCallback(
         (option: IOption) => {
@@ -15,5 +27,11 @@ export const SelectLang: FC = () => {
         [onLangChange],
     );
 
-    return <Select onSelect={handleSelect} options={LANG_OPTIONS} />;
+    return (
+        <Select
+            defaultSelectedOptionIndex={defaultSelectedOptionIndex}
+            onSelect={handleSelect}
+            options={LANG_OPTIONS}
+        />
+    );
 };
