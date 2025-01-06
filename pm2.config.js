7d1f745717e12af5c22ecd887ae7fb39d2244a7b
Yaolegol <m160160@yandex.ru>
2025-01-07 04:19:32 +0700

    Добавлен pm2 конфиг

---
+++ pm2.config.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+    apps: [
+        {
+            env: {
+                NODE_ENV: 'production',
+            },
+            name: 'frutostore',
+            script: 'npm start',
+        },
+    ],
+};
